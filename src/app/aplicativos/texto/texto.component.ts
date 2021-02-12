7e274be4c9c43b6116957ccfd967ab4be0f93264
Cassiano Kruchelski <cass@protonmail.com>
2021-02-11 23:26:49 -0300

    Ajuste de cores no markdown

--- src/app/aplicativos/texto/texto.component.ts
+++ src/app/aplicativos/texto/texto.component.ts
@@ -24,23 +24,6 @@ export class TextoComponent extends AplicativoGenericoComponent implements OnIni
 
   // Formulário;
   form: FormGroup;
-
-  markdown = `## Markdown __rulez__!
----
-
-### Syntax highlight
-\`\`\`typescript
-const language = 'typescript';
-\`\`\`
-
-### Lists
-1. Ordered list
-2. Another bullet point
-  - Unordered list
-  - Another unordered bullet point
-
-### Blockquote
-> Blockquote to the max`;
   
   constructor(
     _appServ: AplicativoService,
